82a9cb234c4f05242f76cae73e7cb25625068377
TonnyJP <tonny.piper@gmx.de>
2023-02-26 09:36:04 +0100

    playing with maping and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+  <main>
+    <div className="container" >
+    <h2>Questions And Answer about login</h2>
+    <section className="info">
+      <ul>
+        {data.map((quest) => {
+          const { id, title, info } = quest;
+          return <li key ={id}>
+            <SingleQuestion title={title} info={info} />
+          </li>
+        })}
+      </ul>
+    </section>
+    </div>
+  </main>)
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title, info}) => {
+
+  const [isTextShown, setIsTextShown ] = React.useState(false)
+
+  const showText = () => {
+    setIsTextShown(!isTextShown)
+  }
+
+  return (
+  <article className="question">
+    <header>
+      <h4>{title}</h4> <button className="btn" onClick={showText}> {isTextShown? <AiOutlineMinus /> : < AiOutlinePlus />} </button>
+    </header>
+    {isTextShown && <p> {info} </p>}
+  </article>)
 };
 
 export default Question;
